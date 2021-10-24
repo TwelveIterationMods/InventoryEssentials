09d69af418e7910bf96b6f3d35ceb60bd594de5c
blay09 <blay09@hotmail.de>
2021-10-24 11:09:34 +0200

    prepare for release

--- forge/src/main/java/net/blay09/mods/inventoryessentials/ForgeInventoryEssentials.java
+++ forge/src/main/java/net/blay09/mods/inventoryessentials/ForgeInventoryEssentials.java
@@ -6,7 +6,10 @@ import net.blay09.mods.inventoryessentials.client.InventoryEssentialsClient;
 import net.minecraft.world.inventory.Slot;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
 import net.minecraftforge.items.SlotItemHandler;
 
 import java.util.Optional;
@@ -28,6 +31,8 @@ public class ForgeInventoryEssentials {
 
         Balm.initialize(InventoryEssentials.MOD_ID, InventoryEssentials::initialize);
         DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> BalmClient.initialize(InventoryEssentials.MOD_ID, InventoryEssentialsClient::initialize));
+
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 
 }
