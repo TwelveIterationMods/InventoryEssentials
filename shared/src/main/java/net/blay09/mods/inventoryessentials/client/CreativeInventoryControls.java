7a3d8e5eaa2c5e102dd92412b19bfa9dca29ecf6
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-01-14 12:04:08 +0100

    fix: Fix crash when shift-dragging in creative mode #27

--- shared/src/main/java/net/blay09/mods/inventoryessentials/client/CreativeInventoryControls.java
+++ shared/src/main/java/net/blay09/mods/inventoryessentials/client/CreativeInventoryControls.java
@@ -2,9 +2,11 @@ package net.blay09.mods.inventoryessentials.client;
 
 import net.blay09.mods.inventoryessentials.mixin.SlotWrapperAccessor;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ClickType;
+import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraft.world.inventory.Slot;
 
 public class CreativeInventoryControls extends ClientOnlyInventoryControls {
@@ -16,7 +18,10 @@ public class CreativeInventoryControls extends ClientOnlyInventoryControls {
     @Override
     protected void slotClick(AbstractContainerMenu menu, Slot slot, int mouseButton, ClickType clickType) {
         if (slot instanceof SlotWrapperAccessor accessor) {
-            slotClick(menu, accessor.getTarget().index, mouseButton, clickType);
+            final var player = Minecraft.getInstance().player;
+            if (player != null) {
+                slotClick(player.inventoryMenu, accessor.getTarget().index, mouseButton, clickType);
+            }
         } else {
             slotClick(menu, slot.index, mouseButton, clickType);
         }
@@ -24,8 +29,10 @@ public class CreativeInventoryControls extends ClientOnlyInventoryControls {
 
     @Override
     protected void slotClick(AbstractContainerMenu menu, int slotIndex, int mouseButton, ClickType clickType) {
-        Minecraft minecraft = Minecraft.getInstance();
-        minecraft.player.inventoryMenu.clicked(slotIndex, mouseButton, clickType, minecraft.player);
-        minecraft.player.inventoryMenu.broadcastChanges();
+        final var player = Minecraft.getInstance().player;
+        if (player != null) {
+            menu.clicked(slotIndex, mouseButton, clickType, player);
+            menu.broadcastChanges();
+        }
     }
 }
