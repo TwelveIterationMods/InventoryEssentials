027b79c5d8cf5b06f49563a14b37897fecc5c511
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-03-17 20:45:00 +0100

    feat: Update to Minecraft 1.21.5

--- common/src/main/java/net/blay09/mods/inventoryessentials/InventoryEssentialsConfig.java
+++ common/src/main/java/net/blay09/mods/inventoryessentials/InventoryEssentialsConfig.java
@@ -1,14 +1,27 @@
 package net.blay09.mods.inventoryessentials;
 
 import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.api.config.reflection.Comment;
+import net.blay09.mods.balm.api.config.reflection.Config;
 
+@Config(InventoryEssentials.MOD_ID)
 public class InventoryEssentialsConfig {
-    public static InventoryEssentialsConfigData getActive() {
-        return Balm.getConfig().getActive(InventoryEssentialsConfigData.class);
+
+    @Comment("Use the client implementation even on servers that have the mod installed - only useful for development purposes.")
+    public boolean forceClientImplementation;
+
+    @Comment("Should space-clicking move all items even if an empty slot was clicked?")
+    public boolean allowBulkTransferAllOnEmptySlot = false;
+
+    @Comment("Should space-clicking armor in the inventory swap to all matching armor?")
+    public boolean bulkTransferArmorSets = true;
+
+    public static InventoryEssentialsConfig getActive() {
+        return Balm.getConfig().getActiveConfig(InventoryEssentialsConfig.class);
     }
 
     public static void initialize() {
-        Balm.getConfig().registerConfig(InventoryEssentialsConfigData.class, null);
+        Balm.getConfig().registerConfig(InventoryEssentialsConfig.class);
     }
 }
 
