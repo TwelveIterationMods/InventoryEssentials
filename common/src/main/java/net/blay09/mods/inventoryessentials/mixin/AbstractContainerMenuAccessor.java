f93b9a8596461b468bd76214c66f6ca9d8cbd4ff
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-09-08 20:51:36 +0200

    fix: Fix crash on screens with no real menu behind them #53
    # Conflicts:
    #	forge/src/main/resources/inventoryessentials.mixins.json
    #	gradle.properties

---
+++ common/src/main/java/net/blay09/mods/inventoryessentials/mixin/AbstractContainerMenuAccessor.java
@@ -0,0 +1,12 @@
+package net.blay09.mods.inventoryessentials.mixin;
+
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(AbstractContainerMenu.class)
+public interface AbstractContainerMenuAccessor {
+    @Accessor("menuType")
+    MenuType<?> balm$getMenuType();
+}
