520c2fabc3e7cd4fbf51882400fbcd4b4e394ceb
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-06-19 13:25:00 +0200

    fix: Fix consolidateStacks sending slot click packets even if no moves are to be made

--- common/src/main/java/net/blay09/mods/inventoryessentials/client/ClientInventorySorting.java
+++ common/src/main/java/net/blay09/mods/inventoryessentials/client/ClientInventorySorting.java
@@ -114,6 +114,11 @@ public class ClientInventorySorting {
 
             final var thisStack = thisSlot.getItem();
             for (int j = i + 1; j < slots.size(); j++) {
+                final int thisStackLimit = Math.min(thisSlot.getMaxStackSize(), thisSlot.getMaxStackSize(thisStack));
+                if (thisStack.getCount() >= thisStackLimit) {
+                    break;
+                }
+
                 final var otherSlot = slots.get(j);
                 final var otherStack = otherSlot.getItem();
                 if (!otherStack.isEmpty() && ItemStack.isSameItemSameTags(thisStack, otherStack)) {
@@ -121,12 +126,6 @@ public class ClientInventorySorting {
                     clicker.click(menu, thisSlot, 0, ClickType.PICKUP);
                     if (!menu.getCarried().isEmpty()) {
                         clicker.click(menu, otherSlot, 0, ClickType.PICKUP);
-                    }
-
-                    final var newThisStack = thisSlot.getItem();
-                    final int newThisStackFull = newThisStack.getMaxStackSize();
-                    if (newThisStack.getCount() >= newThisStackFull) {
-                        break;
                     }
                 }
             }
